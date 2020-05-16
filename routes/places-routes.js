307c448fbd8ee8b6ea62e56c4441054e76fc1752
unknown <achraf.rehioui.1@gmail.com>
2020-05-16 22:05:33 +0200

    Getting place by Id

---
+++ routes/places-routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Test',
+    description: 'Test',
+    location: {
+      lat: 40,
+      lng: -73
+    },
+    address: 'Test',
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({place}); // => { place } => { place: place }
+});
+
+module.exports = router;
