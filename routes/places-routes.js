56c4fbd24341e9f6f4c32d449eaed398968a0d31
unknown <achraf.rehioui.1@gmail.com>
2020-05-17 23:33:32 +0200

    Place By Id

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
+const D_PLACES = [
   {
     id: 'p1',
     title: 'Test',
@@ -18,10 +18,22 @@ const DUMMY_PLACES = [
 
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find(p => {
+  const place = D_PLACES.find(p => {
     return p.id === placeId;
   });
   res.json({place}); // => { place } => { place: place }
+});
+
+router.get('/user/:uid', (req, res, next) => {
+
+  const userId = req.params.uid;
+
+  const place = D_PLACES.find( p => {
+
+    return p.creator === userId;
+  });
+
+  res.json({ place });
 });
 
 module.exports = router;
